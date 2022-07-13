9ed65c9a80f4c0d406acc431bb55a03c1e3b7c90
Richard Masci <rmasci@Richs-MacBook-Pro.local>
2022-07-13 08:22:46 -0400

    Added bootstrap so it's served statically

--- routes.go
+++ routes.go
@@ -10,5 +10,7 @@ func (conf *Conf) routes() http.Handler {
 
 	mux.Get("/", http.HandlerFunc(conf.Home))
 	mux.Get("/ws", http.HandlerFunc(WsEndpoint))
+	fileServer := http.FileServer(http.Dir("./static"))
+	mux.Get("/static/", http.StripPrefix("/static", fileServer))
 	return mux
 }
